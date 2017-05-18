20ef573546303dc6f7014814a864d713f012ee48
Peter Williams <peter@newton.cx>
2017-05-18 12:22:50 -0400

    slides: fix PDF aspect ratio; tweak final admonitions

--- slides/pdf-export.sh
+++ slides/pdf-export.sh
@@ -4,5 +4,5 @@ exec docker run \
      --net=host \
      -v $(pwd):/slides:rw,Z \
      astefanutti/decktape \
-     -s 1280x720 \
+     -s 960x720 \
      reveal http://localhost:23232/slides.html?pdf-print export.pdf
